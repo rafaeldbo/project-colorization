974c0f7002b5cbb8c71330eefdd0fa285d0df3a2
rafaeldbo <rafaeldbo@al.insper.edu.br>
2024-11-14 19:44:39 -0300

    Usando Multiprocess

--- tcnn/training.py
+++ tcnn/training.py
@@ -1,42 +1,61 @@
+import time
+from os import mkdir, path
 
-import time, torch
-
-from os import path, mkdir
-from torch import optim, nn, save, load
+import torch
+from torch import load, nn, optim, save
 from torch.utils.data import DataLoader
 
 from .dataset import ImageDataset
 
 
-def train_model(model:nn.Module, model_name:str, images_folder:str, dir:str='./', 
-                from_checkpoint:bool=False, checkpoint_period:int=25,
-                amount_images:int=4, epochs:int=50, learning_rate:float=0.001, 
-                cuda:bool=True, batch_size:int=32, prefetch_factor:int=None, num_workers:int=0
-    ) -> None:
+def train_model(
+    model: nn.Module,
+    model_name: str,
+    images_folder: str,
+    dir: str = './',
+    from_checkpoint: bool = False,
+    checkpoint_period: int = 25,
+    amount_images: int = 4,
+    epochs: int = 50,
+    learning_rate: float = 0.001,
+    batch_size: int = 32,
+    cuda: bool = True,
+    pin_memory: bool = False,
+    prefetch_factor: int = None,
+    num_workers: int = 0,
+) -> None:
 
     # Checking multiprocessing parameters are valid.
-    pin_memory = False
-    if prefetch_factor is not None:
-        if num_workers > 0:
-            pin_memory = True
-        else:
-            raise Exception("You need to have at least one worker to use the prefetch_factor.")
-        
+    # if prefetch_factor is not None:
+    #     if num_workers > 0:
+    #         pin_memory = True
+    #     else:
+    #         raise Exception(
+    #             "You need to have at least one worker to use the prefetch_factor."
+    #         )
+
     # Loading Dataset and creating the corresponding DataLoader.
     data = ImageDataset(dir, images_folder, amount_images)
-    dataloader = DataLoader(data, batch_size=batch_size, shuffle=True, pin_memory=pin_memory, prefetch_factor=prefetch_factor, num_workers=num_workers)
+    dataloader = DataLoader(
+        data,
+        batch_size=batch_size,
+        shuffle=True,
+        pin_memory=pin_memory,
+        prefetch_factor=prefetch_factor,
+        num_workers=num_workers,
+    )
 
     # Creating folders to store the checkpoints and models.
-    checkpoints_dir = path.join(dir, "checkpoints")        
+    checkpoints_dir = path.join(dir, "checkpoints")
     if not path.exists(checkpoints_dir):
         mkdir(checkpoints_dir)
         from_checkpoint = False
     checkpoint_file = path.join(checkpoints_dir, f"{model_name}_checkpoint.pt")
-    
+
     models_dir = path.join(dir, "models")
     if not path.exists(models_dir):
         mkdir(models_dir)
-        
+
     # choosing the device
     device_name = "cuda" if torch.cuda.is_available() and cuda else "cpu"
     print(f"Using {device_name}")
@@ -72,11 +91,11 @@ def train_model(model:nn.Module, model_name:str, images_folder:str, dir:str='./'
         epoch_running_loss = 0
         for i, data in enumerate(dataloader):
             gray, color, category = data
-            
+
             gray = gray.to(device)
             color = color.to(device)
             category = category.to(device)
-            
+
             optimizer.zero_grad()
             outputs = cnn(gray, category, category)
 
@@ -85,25 +104,30 @@ def train_model(model:nn.Module, model_name:str, images_folder:str, dir:str='./'
             optimizer.step()
 
             epoch_running_loss += loss.item()
-            print(f'[{epoch+1:2d}, {i + 1:2d}] loss: {loss.item()/2000:.3f}')
+            print(f'[{epoch+1:2d}, {i + 1:3d}] loss: {loss.item()/2000:.3f}')
         running_losses.append(epoch_running_loss)
-        
+
         # Saving the model every checkpoint_period epochs.
-        if epoch % checkpoint_period == 0 and epoch > epochs:
-            save({
-                "epoch": epoch,
-                "model_state_dict": cnn.state_dict(),
-                "optimizer_state_dict": optimizer.state_dict(),
-                "time": time.time() - start + initial_time,
-                "running_losses": running_losses
-            }, f"./checkpoints/{model_name}_checkpoint.pt")
-            
+        if (epoch % checkpoint_period == 0) and (epoch < epochs) and (epoch != 0):
+            print('Saving checkpoint')
+            save(
+                {
+                    "epoch": epoch,
+                    "model_state_dict": cnn.state_dict(),
+                    "optimizer_state_dict": optimizer.state_dict(),
+                    "time": time.time() - start + initial_time,
+                    "running_losses": running_losses,
+                },
+                f"./checkpoints/{model_name}_checkpoint.pt",
+            )
+
     print("Finished")
     # Saving the final model.
-    save({
-        "epoch": epoch,
-        "model_state_dict": cnn.state_dict(),
-        "optimizer_state_dict": optimizer.state_dict(),
-        "time": time.time() - start + initial_time,
-        "running_losses": running_losses
-    }, f"./models/{model_name}.pt")
+    save(
+        {
+            "epoch": epoch,
+            "model_state_dict": cnn.state_dict(),
+            "optimizer_state_dict": optimizer.state_dict(),
+            "time": time.time() - start + initial_time,
+            "running_losses": running_losses
+        }, f"./models/{model_name}.pt")
--- tcnn_main.py
+++ tcnn_main.py
@@ -3,19 +3,32 @@ from tcnn.testing import test_model
 from tcnn.network import NetworkBasic, NetworkAdvanced
 from os import path
 
+
 def main():
     # Get the directory of the current file
-    dir = path.dirname(path.abspath(__file__))
-    print(f"Current directory: {dir}")
+    dir_ = path.dirname(path.abspath(__file__))
+    print(f"Current directory: {dir_}")
 
     model = NetworkAdvanced
-    model_name = "tcnn_basic"
-    
+    model_name = "tcnn_advanced_1024"
+
     # Make sure you have already downloaded the data mentioned in the README file.
-    train_model(model, model_name, "train_color", dir, amount_images=64)
+    train_params = {
+        'model': model,
+        'model_name': model_name,
+        'images_folder': "train_color",
+        'dir': dir_,
+        'amount_images': 1024,
+        'learning_rate': 0.01,
+        'pin_memory': True,
+        'prefetch_factor': 4,
+        'num_workers': 4,
+    }
+    train_model(**train_params)
 
     # Puts to the test the final model.
-    test_model(model, model_name, "test_color", dir)
-    
+    test_model(model, model_name, "test_color", dir_)
+
+
 if __name__ == "__main__":
     main()
