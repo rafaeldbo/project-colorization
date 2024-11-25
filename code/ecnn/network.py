9d587193eabdbc47ae9a42d78640cdfb409d62ba
kadu-ymd <carlosepy@al.insper.edu.br>
2024-11-25 19:29:01 -0300

    Alterações no código da network e documentação

--- code/ecnn/network.py
+++ code/ecnn/network.py
@@ -2,8 +2,9 @@ from torch import nn, Tensor, cat
 from torch.nn.functional import relu
 import torch
 
+
 class NetworkBasic(nn.Module):
-    def __init__(self, number_categories:int=8, emb_size:int=10) -> None:
+    def __init__(self, number_categories: int = 8, emb_size: int = 10) -> None:
         """
         Initializes each part of the convolutional neural network.
         """
@@ -14,28 +15,44 @@ class NetworkBasic(nn.Module):
         self.embb = nn.Embedding(number_categories, emb_size)
 
         # Encoder.
-        self.conv1 = nn.Conv2d(1 + self.emb_size, 32, kernel_size=4, stride=2, padding=1)
+        self.conv1 = nn.Conv2d(
+            1 + self.emb_size, 32, kernel_size=4, stride=2, padding=1
+        )
         self.conv1_bn = nn.BatchNorm2d(32)
         self.conv2 = nn.Conv2d(32, 64, kernel_size=4, stride=2, padding=1)
         self.conv2_bn = nn.BatchNorm2d(64)
 
         # Decoder.
-        self.tconv1 = nn.ConvTranspose2d(64 + self.emb_size, 32, kernel_size=4, stride=2, padding=1)
+        self.tconv1 = nn.ConvTranspose2d(
+            64 + self.emb_size, 32, kernel_size=4, stride=2, padding=1
+        )
         self.tconv1_bn = nn.BatchNorm2d(32)
         self.tconv2 = nn.ConvTranspose2d(32, 2, kernel_size=4, stride=2, padding=1)
 
-    def forward(self, gray:Tensor, category_in:int, category_out:int) -> Tensor:
-        
+    def forward(
+        self,
+        gray: Tensor,
+        category: int,
+    ) -> Tensor:
+
         # First Embeeding.
-        embb_category_in = self.embb(category_in).view(-1, self.emb_size, 1, 1).repeat(1, 1, gray.shape[2], gray.shape[3])
+        embb_category_in = (
+            self.embb(category)
+            .view(-1, self.emb_size, 1, 1)
+            .repeat(1, 1, gray.shape[2], gray.shape[3])
+        )
         gray_embb_in = cat((gray, embb_category_in), 1)
 
         # Encoder.
         gray_conv1 = relu(self.conv1_bn(self.conv1(gray_embb_in)))
         gray_conv2 = relu(self.conv2_bn(self.conv2(gray_conv1)))
-        
+
         # Second Embeeding.
-        embb_category_out = self.embb(category_out).view(-1, self.emb_size, 1, 1).repeat(1, 1, gray_conv2.shape[2], gray_conv2.shape[3])
+        embb_category_out = (
+            self.embb(category)
+            .view(-1, self.emb_size, 1, 1)
+            .repeat(1, 1, gray_conv2.shape[2], gray_conv2.shape[3])
+        )
         gray_embb_out = cat((gray_conv2, embb_category_out), 1)
 
         # Decoder.
@@ -43,8 +60,13 @@ class NetworkBasic(nn.Module):
         gray_tconv2 = relu(self.tconv2(gray_tconv1))
         return gray_tconv2
 
+
 class NetworkAdvanced(nn.Module):
-    def __init__(self, number_categories:int=8, emb_size:int=10) -> None:
+    def __init__(
+        self,
+        number_categories: int = 8,
+        emb_size: int = 10,
+    ) -> None:
         """
         Initializes each part of the convolutional neural network.
         """
@@ -53,9 +75,11 @@ class NetworkAdvanced(nn.Module):
         # Embedding.
         self.emb_size = emb_size
         self.embb = nn.Embedding(number_categories, emb_size)
-        
+
         # Encoder.
-        self.conv1 = nn.Conv2d(1 + self.emb_size, 32, kernel_size=4, stride=2, padding=1)
+        self.conv1 = nn.Conv2d(
+            1 + self.emb_size, 32, kernel_size=4, stride=2, padding=1
+        )
         self.conv1_bn = nn.BatchNorm2d(32)
         self.conv2 = nn.Conv2d(32, 64, kernel_size=4, stride=2, padding=1)
         self.conv2_bn = nn.BatchNorm2d(64)
@@ -65,13 +89,19 @@ class NetworkAdvanced(nn.Module):
         self.conv4_bn = nn.BatchNorm2d(256)
 
         # Dilation.
-        self.conv5 = nn.Conv2d(256, 256, kernel_size=4, stride=1, padding=3, dilation=2)
-        self.conv5_bn = nn.BatchNorm2d(256)
-        self.dilat = nn.Conv2d(256, 256, kernel_size=4, stride=1, padding=3, dilation=2)
-        self.dilat_bn = nn.BatchNorm2d(256)
+        self.dilat1 = nn.Conv2d(
+            256, 256, kernel_size=4, stride=1, padding=3, dilation=2
+        )
+        self.dilat1_bn = nn.BatchNorm2d(256)
+        self.dilat2 = nn.Conv2d(
+            256, 256, kernel_size=4, stride=1, padding=3, dilation=2
+        )
+        self.dilat2_bn = nn.BatchNorm2d(256)
 
         # Decoder.
-        self.tconv1 = nn.ConvTranspose2d(256 + self.emb_size, 128, kernel_size=4, stride=2, padding=1)
+        self.tconv1 = nn.ConvTranspose2d(
+            256 + self.emb_size, 128, kernel_size=4, stride=2, padding=1
+        )
         self.tconv1_bn = nn.BatchNorm2d(128)
         self.tconv2 = nn.ConvTranspose2d(256, 64, kernel_size=4, stride=2, padding=1)
         self.tconv2_bn = nn.BatchNorm2d(64)
@@ -81,25 +111,37 @@ class NetworkAdvanced(nn.Module):
 
         self.output = nn.Conv2d(3, 2, kernel_size=3, stride=1, padding=1)
 
-    def forward(self, gray:Tensor, category_in:int, category_out:int) -> Tensor:
-        
+    def forward(
+        self,
+        gray: Tensor,
+        category: int,
+    ) -> Tensor:
+
         # First Embeeding.
-        embb_category_in = self.embb(category_in).view(-1, self.emb_size, 1, 1).repeat(1, 1, gray.shape[2], gray.shape[3])
+        embb_category_in = (
+            self.embb(category)
+            .view(-1, self.emb_size, 1, 1)
+            .repeat(1, 1, gray.shape[2], gray.shape[3])
+        )
         gray_embb_in = cat((gray, embb_category_in), 1)
-        
+
         # Encoder.
         gray_conv1 = relu(self.conv1_bn(self.conv1(gray_embb_in)))
         gray_conv2 = relu(self.conv2_bn(self.conv2(gray_conv1)))
         gray_conv3 = relu(self.conv3_bn(self.conv3(gray_conv2)))
         gray_conv4 = relu(self.conv4_bn(self.conv4(gray_conv3)))
-        
+
         # Dilation.
-        gray_conv5 = relu(self.conv5_bn(self.conv5(gray_conv4)))
-        gray_dilat5 = relu(self.dilat_bn(self.dilat(gray_conv5)))
+        gray_dilat1 = relu(self.dilat1_bn(self.dilat1(gray_conv4)))
+        gray_dilat2 = relu(self.dilat2_bn(self.dilat2(gray_dilat1)))
 
         # Second Embeeding.
-        embb_category_out = self.embb(category_out).view(-1, self.emb_size, 1, 1).repeat(1, 1, gray_dilat5.shape[2], gray_dilat5.shape[3])
-        gray_embb_out = cat((gray_dilat5, embb_category_out), 1)
+        embb_category_out = (
+            self.embb(category)
+            .view(-1, self.emb_size, 1, 1)
+            .repeat(1, 1, gray_dilat2.shape[2], gray_dilat2.shape[3])
+        )
+        gray_embb_out = cat((gray_dilat2, embb_category_out), 1)
 
         # Decoder.
         gray_tconv1 = relu(self.tconv1_bn(self.tconv1(gray_embb_out)))
@@ -110,7 +152,6 @@ class NetworkAdvanced(nn.Module):
         gray_tconv3 = cat((gray_tconv3, gray_conv1), 1)
         gray_tconv4 = relu(self.tconv4(gray_tconv3))
         gray_tconv4 = cat((gray_tconv4, gray), 1)
-        
+
         output = self.output(gray_tconv4)
         return output
-
